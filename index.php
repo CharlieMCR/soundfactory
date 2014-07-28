e584c73e815c46627f8d66b94ff2c47a30fa7786
CharlieMCR <charlieheesom@Charlie-Heesoms-iMac.local>
2014-07-28 16:38:48 +0100

    initial files

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Soundfactory Development</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="xcss/reset.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body id="home">
+
+    <div class="wrapper">
+        <header>
+
+            <h1 class="logo"><a href="">Soundfactory</a></h1>
+
+            <a class="to_nav" href="#primary_nav">Menu</a>
+
+        </header>
+
+        <article>
+            <h2>Soundfactory Development</h2>
+
+            <p>Lorem ipsum dolor sit amet.</p>
+
+        </article>
+
+        <article>
+            <h2>Introduction to Soundfactory</h2>
+
+            <p>Principles and work practices</p>
+
+        </article>
+
+        <article>
+            
+        <h2>Stuff that been done</h2>
+
+        <p>See, this stuff right here.</p>
+
+        </article>
+
+        <footer>
+            
+        <p>Copyright &copy;2014 Soundfactory</p>
+
+        </footer>
+
+        <nav id="primary_nav">
+
+            <ul>
+
+                <li><a href="">About</a></li>
+
+                <li><a href="">Projects</a></li>
+
+                <li><a href="">Contact</a></li>
+
+                <li><a href="">Top</a></li>
+            </ul>
+
+        </nav>
+    </div>
+
+</body>
+</html>
