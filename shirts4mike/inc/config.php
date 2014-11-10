7a6c336dfbe3d3988adecec30a2ce674caa3dad5
CharlieMCR <charlieheesom@gmail.com>
2014-11-10 18:17:21 +0000

    updated file structure, no longer in subfolder

--- shirts4mike/inc/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-	define("BASE_URL","/shirts4mike/");
-	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/shirts4mike/");
--- shirts4mike/inc/partial-product-list-view.html.php
+++
@@ -1,6 +0,0 @@
-<li>
-        <a href="<?php echo BASE_URL; ?>shirts/<?php echo $product["sku"]; ?>/">
-            <img src="<?php echo BASE_URL . $product["img"]; ?>" alt="<?php echo $product["name"]; ?>">
-            <p>View Details</p>
-        </a>
-    </li>
