cf4c721eed7c82d473d2fd3ba44eaa26faf6a53f
CharlieMCR <charlieheesom@gmail.com>
2014-11-11 11:01:00 +0000

    updated styling for footer, nav and product page clearfix

--- catalog/product.php
+++ catalog/product.php
@@ -42,6 +42,12 @@
 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 					<p class="note">* All Images supplied by <a href="http://lorempixel.com/">Lorempixel.com</a></p>
 					<p class="note">** All monetary values are made up.</p>
